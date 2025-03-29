0da156db92b6207b461517e72df224cec9063f41
MarcWeberFS <marc.robin.weber@gmail.com>
2025-03-29 21:11:10 +0100

    init

---
+++ text-to-sql/src/main/java/ch/zhaw/text_to_sql/TextToSqlApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.text_to_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToSqlApplication.class, args);
+	}
+
+}
---
+++ text-to-sql/src/test/java/ch/zhaw/text_to_sql/TextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.text_to_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
