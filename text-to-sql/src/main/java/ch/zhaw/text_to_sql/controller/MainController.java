85d9e48937acffb86d8c833de736e03bce55a0b8
MarcWeberFS <marc.robin.weber@gmail.com>
2025-05-28 10:35:40 +0200

    READ.ME hinzugefügt

--- text-to-sql/src/main/java/ch/zhaw/text_to_sql/controller/MainController.java
+++ text-to-sql/src/main/java/ch/zhaw/text_to_sql/controller/MainController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
