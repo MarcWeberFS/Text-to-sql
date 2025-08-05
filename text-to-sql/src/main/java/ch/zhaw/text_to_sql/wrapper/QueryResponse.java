db299716b9762e488071b5eb3d2e3fb902cbb5d8
MarcWeberFS <marc.robin.weber@gmail.com>
2025-08-05 16:01:26 +0200

    code format

--- text-to-sql/src/main/java/ch/zhaw/text_to_sql/wrapper/QueryResponse.java
+++ text-to-sql/src/main/java/ch/zhaw/text_to_sql/wrapper/QueryResponse.java
@@ -12,8 +12,6 @@ public class QueryResponse {
         private boolean improved;
         private List<Map<String, Object>> result;
     
-
-
         public QueryResponse(String executedQuery, int retries, boolean improved, List<Map<String, Object>> result) {
             this.executedQuery = executedQuery;
             this.retries = retries;
